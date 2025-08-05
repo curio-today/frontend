6adfe11bbab0ad4088caf0fe110b0f23be98fb87
Dmytro <dmytropolizhai@gmail.com>
2025-08-05 13:08:08 +0300

    feat(props): moved all props to shared folder

--- src/components/ui/buttons/props.ts
+++
@@ -1,6 +0,0 @@
-import { IconProps } from "@/components/ui/Icon/props";
-
-export type ButtonProps = {
-    text: string;
-    icon?: IconProps;
-}
---
+++ src/shared/props/ButtonProps.ts
@@ -0,0 +1,6 @@
+import { IconProps } from "@/shared/props/IconProps";
+import { TextProps } from "@/shared/props/TextProps";
+
+
+export type ButtonProps =
+    TextProps & { icon?: IconProps };
