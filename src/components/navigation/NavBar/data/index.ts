5ef50c3e37a81bd47130b0d5c0c53a3b779f9259
Dmytro <dmytropolizhai@gmail.com>
2025-08-09 23:28:49 +0300

    fix(nav-link-data): added data for navigation links

---
+++ src/components/navigation/NavBar/data/index.ts
@@ -0,0 +1,29 @@
+import { NavBarData } from "@/components/navigation/NavBar/types/NavBarData";
+
+export const NavLinkData: NavBarData[] = [
+    {
+        href: "/amazes",
+        title: "Amazes",
+        icon: "wow",
+    },
+    {
+        href: "/informs",
+        title: "Informs",
+        icon: "megaphone",
+    },
+    {
+        href: "/",
+        title: "Feed",
+        icon: "newspaper",
+    },
+    {
+        href: "/amuses",
+        title: "Amuses",
+        icon: "clock",
+    },
+    {
+        href: "/inspires",
+        title: "Inspires",
+        icon: "lightbulb",
+    },
+]
