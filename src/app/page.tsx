d308a18abe8ce49eb8a1275e300820f875bba6d7
Dmytro <dmytropolizhai@gmail.com>
2025-08-09 23:39:16 +0300

    feat(page): added the elements for playground testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
 import styles from "./page.module.css";
 
-import { Button } from "@/components/ui/Button";
-
+import Button from "@/components/ui/Button";
+import List from "@/components/layout/List";
+import Text from "@/components/ui/Text";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-          <Button text="Click me!" />
-          <h1>Smart Curiosity</h1>
-          <h2>Smart Curiosity</h2>
-          <h3>Smart Curiosity</h3>
-          <p>Smart Curiosity</p>
-          <small>Smart Curiosity</small>
+          <List orientation="vertical">
+              <Button title="Click me!" mode="normal" />
+              <Button title="Click me with icon!" mode="normal" icon="globe" />
+              <Button mode="normal" icon="globe" />
+              <Button mode="outline" icon="globe" title="Language"/>
+              <Button mode="fill" icon="globe" />
+          </List>
+
+          <Text variant="h1">Smart Curiosity</Text>
+          <Text variant="h2">Smart Curiosity</Text>
+          <Text variant="h3">Smart Curiosity</Text>
+          <Text variant="p">Smart Curiosity</Text>
+          <Text variant="small">Smart Curiosity</Text>
       </main>
     </div>
   );
