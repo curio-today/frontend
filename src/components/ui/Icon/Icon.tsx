fc176fd9b965fcc50dc96160aa788268c8b8170a
Dmytro <dmytropolizhai@gmail.com>
2025-08-09 10:59:05 +0300

    feat(kit): icon is ready

---
+++ src/components/ui/Icon/Icon.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+
+import styles from "./Icon.module.scss"
+import { IconProps } from "./types/IconProps";
+import { IconData } from "./data";
+
+import clsx from "clsx";
+
+/**
+ * Renders a static (non-animated) icon.
+ *
+ * @param icon - The type or name of the icon to display.
+ * @param size - The size of the icon.
+ * @param style - Optional inline styles to apply to the icon.
+ * @param props - Additional props to spread onto the icon element.
+ *
+ * @constructor
+ */
+const Icon = ({ icon, size, style, ...props}: IconProps) => {
+    const data = IconData[icon];
+
+    return (
+        <div className={clsx(
+            styles.iconWrapper,
+            styles[size]
+        )} style={style}>
+            <Image
+                src={data.url}
+                alt={data.alt}
+                fill
+                objectFit="contain"
+                priority
+                {...props}
+            />
+        </div>
+    );
+}
+
+export default Icon;
---
+++ src/components/ui/Icon/types/IconType.ts
@@ -0,0 +1,17 @@
+type IconType =
+    | "clock"
+    | "external"
+    | "filter"
+    | "flame"
+    | "globe"
+    | "hashtag"
+    | "magnifying_glass"
+    | "megaphone"
+    | "moon"
+    | "question_mark"
+    | "share"
+    | "sun"
+    | "wow"
+    | "curio";
+
+export default IconType;
