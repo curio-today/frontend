be77d6bee3768cb500a2d0194f486afc0bdf35b7
Dmytro <dmytropolizhai@gmail.com>
2025-08-09 21:30:41 +0300

    feat(list): added 'gap' for changing gap size without using css/scss

--- src/components/layout/List/List.tsx
+++ src/components/layout/List/List.tsx
@@ -1,17 +1,19 @@
 import styles from "./List.module.scss"
 import React, { HTMLAttributes } from "react";
+import { Size } from "@/types/Size";
 
 type ListProps = {
     orientation: "horizontal" | "vertical",
+    gap?: Size,
 } & HTMLAttributes<HTMLUListElement>;
 
-const List = ({ orientation = "vertical", children, className}: ListProps) => {
+const List = ({ orientation = "vertical", children, className = "", gap = "medium"}: ListProps) => {
     const orientationClass = orientation === "vertical"
         ? styles.vertical
         : styles.horizontal;
 
     return (
-        <ul className={`${styles.list} ${orientationClass} ${className}`}>
+        <ul className={`${styles.list} ${orientationClass} ${styles[gap]} ${className}`}>
             {React.Children.map(children, (child, index) => (
                 <li key={index}>{child}</li>
             ))}
