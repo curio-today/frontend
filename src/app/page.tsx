3c0849c9d56948f892d1cc97155b89359c1a0382
Dmytro <dmytropolizhai@gmail.com>
2025-08-10 23:36:10 +0300

    feat(home): added badge for playground

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import Button from "@/components/ui/Button";
 import List from "@/components/layout/List";
 import Text from "@/components/ui/Text";
+import Badge from "@/components/ui/Badge";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
               <Button mode="outline" icon="globe" title="Language"/>
               <Button mode="fill" icon="globe" />
           </List>
+          <Badge icon="wow" label="Amazes" href="/amazes" color="hsla(249, 100%, 65%, 1)" />
 
           <Text variant="h1">Smart Curiosity</Text>
           <Text variant="h2">Smart Curiosity</Text>
