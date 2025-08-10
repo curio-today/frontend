5306703058e38afbf326d613b19e02a46a6b80e5
Dmytro <dmytropolizhai@gmail.com>
2025-08-10 09:45:07 +0300

    feat(nav-link-data): added a mobile and desktop data for links

--- src/components/navigation/NavBar/data/index.ts
+++ src/components/navigation/NavBar/data/index.ts
@@ -1,6 +1,29 @@
 import { NavBarData } from "@/components/navigation/NavBar/types/NavBarData";
 
-export const NavLinkData: NavBarData[] = [
+export const Desktop_NavLinkData: NavBarData[] = [
+    {
+        href: "/amazes",
+        title: "Amazes",
+        icon: "wow",
+    },
+    {
+        href: "/informs",
+        title: "Informs",
+        icon: "megaphone",
+    },
+    {
+        href: "/amuses",
+        title: "Amuses",
+        icon: "clock",
+    },
+    {
+        href: "/inspires",
+        title: "Inspires",
+        icon: "lightbulb",
+    },
+]
+
+export const Mobile_NavLinkData: NavBarData[] = [
     {
         href: "/amazes",
         title: "Amazes",
