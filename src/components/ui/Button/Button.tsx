41f8658bdf54a7491f1ef479337f5b696487e3ee
Dmytro <dmytropolizhai@gmail.com>
2025-08-12 09:08:03 +0300

    fix(button): textVariant was renamed to titleVariant
    feat(button): created a custom classname for overriding style

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,6 +1,7 @@
 import styles from "./Button.module.scss"
 
 import dynamic from "next/dynamic";
+import clsx from "clsx";
 
 import { ButtonProps } from "./types/ButtonProps";
 
@@ -10,15 +11,16 @@ const Text = dynamic(() => import("@/components/ui/Text"));
 const Button = ({ icon, title, children,
                     mode = "normal",
                     iconSize = "medium",
-                    textVariant = "p",
+                    titleVariant = "p",
+                    className,
                     ...rest }: ButtonProps) => {
     return (
-        <button className={styles[mode]} {...rest}>
+        <button className={clsx(styles[mode], className)} {...rest}>
             {icon && (
                 <Icon icon={icon} size={iconSize}></Icon>
             )}
             {title && (
-                <Text variant={textVariant}>{title}</Text>
+                <Text variant={titleVariant}>{title}</Text>
             )}
             {children}
         </button>
