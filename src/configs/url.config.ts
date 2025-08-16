5f28be9af5206233e0a8c627abe0f962e1d35aeb
Dmytro <dmytropolizhai@gmail.com>
2025-08-16 15:15:18 +0300

    feat(url.config): created a function to return admin url with endpoint using an Endpoint type

--- src/configs/url.config.ts
+++ src/configs/url.config.ts
@@ -1,6 +1,10 @@
 import { Heading } from "@/shared/data.types";
+import { Endpoint } from "@/shared/network";
 
 export function getHeadingUrl(heading: Heading, url: string): string {
     return `/${heading}/${url}`
 }
 
+export function getAdmin(): (endpoint: Endpoint) => string {
+    return (endpoint: Endpoint) => (`https://admin.curio.today${endpoint}`)
+}
