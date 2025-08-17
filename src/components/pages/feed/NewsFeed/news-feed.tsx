784491ac69a07c798fbae56cd2835702a8598d71
Dmytro <dmytropolizhai@gmail.com>
2025-08-17 20:38:50 +0300

    fix(news-feed): changed to an infinite scroll

--- src/components/pages/feed/NewsFeed/news-feed.tsx
+++ src/components/pages/feed/NewsFeed/news-feed.tsx
@@ -1,36 +1,31 @@
-import Grid from "@/components/layout/Grid";
-import { getPosts } from "@/lib/api/feed.lib";
-import Post, { PostClient } from "@/components/pages/feed/Post";
+"use client"
 
-export const NewsFeed = async () => {
-    const posts = await getPosts({
+import InfiniteScroll, { useInfiniteFetching } from "@/components/ui/InfiniteScroll";
+import { getPosts } from "@/lib/api/feed.lib";
+import Post, { PostClient, PostProps } from "@/components/pages/feed/Post";
+import Grid from "@/components/layout/Grid";
+
+
+export const NewsFeed = () => {
+    const { data, hasMore, loadMore } = useInfiniteFetching(getPosts, {
         endpoint: "posts",
         page: 1,
         limit: 5,
         _status: "published"
     })
 
-    if (!posts) {
-       return null;
-    }
-
     return (
-        <Grid>
-            <Grid.Row>
-                <Post {...posts[0]} />
-                <Post {...posts[1]} />
-            </Grid.Row>
-            <Grid.Row>
-                <Post {...posts[2]} />
-                <Post {...posts[3]} />
-                <Post {...posts[4]} />
-            </Grid.Row>
-            <Grid.Row>
-                {posts.slice(5).map((post) =>
-                    <PostClient key={post.meta.slug} {...post} />
-                )}
-            </Grid.Row>
-        </Grid>
+        <InfiniteScroll<PostProps> data={data} hasMore={hasMore} loadMore={loadMore}>
+            {(posts) => (
+                <Grid>
+                    {posts.map((post, index) => (
+                        <Grid.Row key={post.meta.slug}>
+                            {index < 2 ? <Post {...post} /> : <PostClient {...post} />}
+                        </Grid.Row>
+                    ))}
+                </Grid>
+            )}
+        </InfiniteScroll>
     )
 }
 
