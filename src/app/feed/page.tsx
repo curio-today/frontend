e8d6940f36859c3488fa31a6fecc987dc297042c
Dmytro <dmytropolizhai@gmail.com>
2025-08-17 15:09:23 +0300

    fix(feed): fetching from page moved to the NewsFeed component

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,14 +1,14 @@
-import { getPosts } from "@/lib/api/feed.lib";
-import Post from "@/components/pages/feed/Post";
+import styles from "./page.module.css"
 
-export default async function Feed() {
-    const posts = await getPosts(5);
+import NewsFeed from "@/components/pages/feed/NewsFeed";
 
+
+export async function Feed() {
     return (
-        <div>
-            {posts?. map((post) => (
-                <Post key={post.meta.slug} {...post} />
-            ))}
+        <div className={styles.page}>
+            <NewsFeed />
         </div>
     )
 }
+
+export default Feed;
