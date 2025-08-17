d79ce952ec038f99cf095364442662fe66030c0f
Dmytro <dmytropolizhai@gmail.com>
2025-08-17 15:08:19 +0300

    fix(fetch-options): added a FetchOptions to index.ts in network

--- src/shared/network/index.ts
+++ src/shared/network/index.ts
@@ -1,3 +1,4 @@
 export type { Endpoint } from "./network.types"
+export type { FetchOptions } from "./request.types"
 export type { ApiResponse } from "./response.types"
 export type { Asset } from "./asset.types"
