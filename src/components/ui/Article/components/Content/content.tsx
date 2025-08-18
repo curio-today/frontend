6933aaadee19f2fb8b8dc293b8f6a020011136e5
Dmytro <dmytropolizhai@gmail.com>
2025-08-18 20:07:32 +0300

    feat(article): Content was created

--- src/components/ui/Article/components/Content/content.tsx
+++ src/components/ui/Article/components/Content/content.tsx
@@ -1,6 +1,12 @@
-const Content = () => {
+import styles from "./content.module.css"
+
+import { ComponentProps } from "react";
+
+type ContentProps = ComponentProps<"section">;
+
+const Content = ({ id = "content", children, ...rest}: ContentProps) => {
     return (
-        <></>
+        <section className={styles.content} id={id} {...rest}>{children}</section>
     )
 }
 
