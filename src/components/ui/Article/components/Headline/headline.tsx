bf0bd9f1df6bd124424346856d423ec515e5e794
Dmytro <dmytropolizhai@gmail.com>
2025-08-18 19:41:57 +0300

    feat(article): headline is created

--- src/components/ui/Article/components/Headline/headline.tsx
+++ src/components/ui/Article/components/Headline/headline.tsx
@@ -1,6 +1,12 @@
-const Headline = () => {
+import styles from "./headline.module.css"
+
+import Text, { TextProps } from "@/components/ui/Text";
+
+type HeadlineProps = TextProps;
+
+const Headline = ({ variant = "h1", id = "headline", children, ...rest }: HeadlineProps) => {
     return (
-        <></>
+        <Text variant={variant} className={styles.headline} id={id} {...rest}>{children}</Text>
     )
 }
 
