99a814b715bdde62b60627d7fd4af700e63d21dc
Dmytro <dmytropolizhai@gmail.com>
2025-08-18 22:53:01 +0300

    feat(converters): created a block converter for custom blocks from the admin panel

---
+++ src/helpers/converters.tsx
@@ -0,0 +1,11 @@
+import { JSXConvertersFunction } from '@payloadcms/richtext-lexical/react';
+import QuoteBlock from "@/components/blocks/QuoteBlock";
+
+export const blockConverters: JSXConvertersFunction = ({ defaultConverters }) => ({
+    ...defaultConverters,
+    blocks: {
+        quote: (node: unknown) => <QuoteBlock text={node.fields.text} author={node.fields.author}/>,
+    },
+
+});
+
