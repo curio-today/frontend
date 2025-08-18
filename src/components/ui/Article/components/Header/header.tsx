0558ab01356f5f06d64c278f848597bac3ae6ea4
Dmytro <dmytropolizhai@gmail.com>
2025-08-18 19:24:08 +0300

    feat(header): header is ready

--- src/components/ui/Article/components/Header/header.tsx
+++ src/components/ui/Article/components/Header/header.tsx
@@ -1,6 +1,14 @@
-const Header = () => {
+import styles from "./header.module.css"
+
+import { ComponentProps, PropsWithChildren } from "react";
+
+export type HeaderProps = PropsWithChildren & ComponentProps<"header">;
+
+const Header = ({ children, ...rest }: HeaderProps) => {
     return (
-        <></>
+        <header className={styles.header} {...rest}>
+            {children}
+        </header>
     )
 }
 
