5afa3a97202e738186b85463868b419ffb0d43b2
Dmytro <dmytropolizhai@gmail.com>
2025-08-18 19:35:30 +0300

    fix(article): createdAt component is ready

--- src/components/ui/Article/components/CreatedAt/created-at.tsx
+++ src/components/ui/Article/components/CreatedAt/created-at.tsx
@@ -1,6 +1,13 @@
-const CreatedAt = () => {
+import styles from "./created-at.module.css"
+
+import Text, { TextProps } from "@/components/ui/Text";
+
+type CreatedAtProps = TextProps;
+
+
+const CreatedAt = ({variant = "h3", ...rest }: CreatedAtProps) => {
     return (
-        <></>
+        <Text variant={variant} className={styles.createdAt} {...rest}>{convertedTime}</Text>
     )
 }
 
