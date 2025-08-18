ba0653dee4e24d66a87e16940ca7d91bd4ed944e
Dmytro <dmytropolizhai@gmail.com>
2025-08-18 23:04:19 +0300

    fix(subtitle): styles was changed from headline styles to subtitle styles. Also, changed a default variant to paragraph because subtitle was too big

--- src/components/ui/Article/components/Subtitle/subtitle.tsx
+++ src/components/ui/Article/components/Subtitle/subtitle.tsx
@@ -1,11 +1,11 @@
 import Text, { TextProps } from "@/components/ui/Text";
-import styles from "@/components/ui/Article/components/Headline/headline.module.css";
+import styles from "./subtitle.module.css";
 
 type SubtitleProps = {
     subtitle: string;
 } & Partial<TextProps>;
 
-const Subtitle = ({ variant = "p", id = "subtitle", subtitle, ...rest }: SubtitleProps) => {
+const Subtitle = ({ variant = "h3", id = "subtitle", subtitle, ...rest }: SubtitleProps) => {
     return (
         <Text variant={variant} className={styles.subtitle} id={id} {...rest}>{subtitle}</Text>
     )
