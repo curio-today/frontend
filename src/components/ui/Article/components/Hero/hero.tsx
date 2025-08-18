b05df025473ea78204968a4cf7871b1fe85411fc
Dmytro <dmytropolizhai@gmail.com>
2025-08-18 20:02:53 +0300

    feat(article): hero is created

--- src/components/ui/Article/components/Hero/hero.tsx
+++ src/components/ui/Article/components/Hero/hero.tsx
@@ -1,16 +1,29 @@
-const HeroImage = () => <></>
-const HeroSource = () => <></>
+import styles from "./hero.module.css"
+
+import Text, { TextProps } from "@/components/ui/Text";
+import { ComponentProps } from "react";
+import Image, { ImageProps } from "next/image";
+
+type HeroProps = ComponentProps<"figure">;
+type HeroImageProps = ImageProps;
+type HeroSourceProps = ComponentProps<"figcaption">;
+
+const HeroImageSource = ({ source, id = "source", variant = "small", ...rest }: { source: string } & Partial<TextProps>) => <Text variant={variant} id={id} {...rest}>{source}</Text>
+const HeroImage = (props: HeroImageProps) => <Image className={styles.image} {...props}/>
 
 
-const Hero = () => {
+const HeroCaption = ({children, ...rest}: HeroSourceProps) => <figcaption className={styles.caption} {...rest}>{children}</figcaption>
+
+const Hero = ({ children, ...rest}: HeroProps ) => {
     return (
-        <></>
+        <figure className={styles.hero} {...rest}>
+            {children}
+        </figure>
     )
 }
-
+HeroImage.Source = HeroImageSource;
 Hero.Image = HeroImage;
-Hero.Source = HeroSource;
-
+Hero.Caption = HeroCaption;
 
 
 
