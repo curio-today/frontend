3b7dfd89b6404b6aa4fb6799f6e569f12e2013af
Dmytro <dmytropolizhai@gmail.com>
2025-08-21 02:54:12 +0300

    fix(grid): RowProps was changed

--- src/components/layout/Grid/grid.types.ts
+++ src/components/layout/Grid/grid.types.ts
@@ -1,7 +1,6 @@
 import { PropsWithChildren } from "react";
 
-import { motion } from "framer-motion";
+import { HTMLMotionProps} from "framer-motion";
 
 export type GridProps = PropsWithChildren;
-
-export type RowProps = typeof motion.div;
+export type RowProps = PropsWithChildren<HTMLMotionProps<"div">>;
