ea95ee1cab5068ea0c94ea44fd939d04d09a1ed6
Dmytro <dmytropolizhai@gmail.com>
2025-08-20 12:49:33 +0300

    feat(config): init

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
+const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
     images: {
         remotePatterns: [new URL('https://admin.curio.today/**')]
     }
-  /* config options here */
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
