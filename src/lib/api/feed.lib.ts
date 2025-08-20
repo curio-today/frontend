01316855f4f95ab338e64e8419f61e918df76117
Dmytro <dmytropolizhai@gmail.com>
2025-08-21 00:43:05 +0300

    fix(feed): removed old logic to fetch localized posts. Now can do by using, for example, "/api/posts?locale=en"

--- src/lib/api/feed.lib.ts
+++ src/lib/api/feed.lib.ts
@@ -1,29 +1,12 @@
 import { Article } from "@/shared/network/content.types";
 import { FetchOptions } from "@/shared/network/";
 import { fetchAdmin } from "@/lib/client.lib";
-import { PaginatedContentResponse } from "@/shared/network/response.types";
 
 
 /**
  * Returns the articles from API by locale.
  * @param options
  */
-export async function getArticlesByLocale(options: FetchOptions): Promise<Article[]> {
-    const data = await fetchAdmin<PaginatedContentResponse<Article>>("/api/posts", options)
-    const localizedArticles: Article[] = [];
-    const ids: string[] = data.docs.map(article => (article.id));
-    ids.forEach((id: string) => {
-        fetchAdmin<Article>(`/api/posts/${id}`, {
-            locale: "ru",
-            limit: 1,
-        })
-            .then(post => {
-                localizedArticles.push(post);
-            })
-            .catch(error => {
-                console.error("Error fetching posts: ", error);
-            })
-    })
-
-    return localizedArticles;
+export async function getArticles(options: FetchOptions): Promise<Article[]> {
+    return await fetchAdmin<Article[]>("/api/posts", options)
 }
