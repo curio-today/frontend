0032cbd902fd2ac7c3a862a0afeb4f42354e672d
Dmytro <dmytropolizhai@gmail.com>
2025-08-21 00:38:32 +0300

    fix(feed): removed old functions. Now only is available

--- src/lib/api/feed.lib.ts
+++ src/lib/api/feed.lib.ts
@@ -1,72 +1,29 @@
-import { PostProps } from "@/components/pages/feed/Post";
-import { getAdmin } from "@/configs/url.config";
-
-import { ApiResponse } from "@/shared/network";
-import { PostAsset } from "@/shared/network/asset.types";
-import { Heading } from "@/shared/data.types";
+import { Article } from "@/shared/network/content.types";
 import { FetchOptions } from "@/shared/network/";
+import { fetchAdmin } from "@/lib/client.lib";
+import { PaginatedContentResponse } from "@/shared/network/response.types";
 
-const API_KEY = "11f7f25b-a955-454f-b551-3f8ba3c72d33";
 
-async function fetchPostsRaw(query?: Partial<FetchOptions>): Promise<PostAsset[]> {
-    const adminUrl = getAdmin();
-    let url = "/api/posts";
+/**
+ * Returns the articles from API by locale.
+ * @param options
+ */
+export async function getArticlesByLocale(options: FetchOptions): Promise<Article[]> {
+    const data = await fetchAdmin<PaginatedContentResponse<Article>>("/api/posts", options)
+    const localizedArticles: Article[] = [];
+    const ids: string[] = data.docs.map(article => (article.id));
+    ids.forEach((id: string) => {
+        fetchAdmin<Article>(`/api/posts/${id}`, {
+            locale: "ru",
+            limit: 1,
+        })
+            .then(post => {
+                localizedArticles.push(post);
+            })
+            .catch(error => {
+                console.error("Error fetching posts: ", error);
+            })
+    })
 
-    if (query?.slug) {
-        url += `?where[slug][equals]=${query.slug}`;
-    } else {
-        const page = query?.page ?? 1;
-        const limit = query?.limit ?? 10;
-        const status = query?._status ?? "published";
-        url += `?page=${page}&limit=${limit}&where[_status][equals]=${status}`;
-    }
-
-    try {
-        const response = await fetch(adminUrl(url), {
-            headers: {
-                Authorization: `third-party-access API-Key ${API_KEY}`,
-            },
-        });
-
-        if (!response.ok) {
-            console.error("Failed to fetch posts:", response.statusText);
-            return Promise.reject(new Error("Failed to fetch posts."));
-        }
-
-        const data: ApiResponse<PostAsset> = await response.json();
-
-        // Fix cover URLs
-        return data.docs.map(doc => ({
-            ...doc,
-            cover: {
-                ...doc.cover,
-                url: adminUrl(doc.cover.url),
-            },
-        }));
-    } catch (error) {
-        console.error("Error fetching posts:", error);
-        return Promise.reject(error);
-    }
-}
-
-export async function getPostBySlug(slug: string): Promise<PostAsset> {
-    const posts = await fetchPostsRaw({ slug });
-    return posts[0];
-}
-
-export async function getPosts(query: FetchOptions): Promise<PostProps[]> {
-    const posts = await fetchPostsRaw(query);
-    return posts.map(doc => ({
-        meta: {
-            heading: doc.badge.name.toLowerCase() as Heading,
-            slug: doc.slug,
-        },
-        image: {
-            src: doc.cover.url,
-            alt: doc.cover.alt,
-        },
-        title: doc.title,
-        subtitle: doc.subtitle,
-        publishedDate: new Date(doc.createdAt),
-    }));
+    return localizedArticles;
 }
