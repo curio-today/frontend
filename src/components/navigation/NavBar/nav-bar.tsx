2963d0242534ef201a4e46de0f3d674a6e5c7747
Dmytro <dmytropolizhai@gmail.com>
2025-08-21 03:49:53 +0300

    fix(nav-bar): nav bar is using namespace headings

--- src/components/navigation/NavBar/nav-bar.tsx
+++ src/components/navigation/NavBar/nav-bar.tsx
@@ -6,13 +6,14 @@ import { Desktop, Mobile } from "@/configs/nav.config";
 import { Server_isMobile } from "@/lib/Platform";
 import { getTranslations } from "next-intl/server";
 import { NavBarHeading } from "@/components/navigation/NavBar/nav-bar.types";
+import { Namespaces } from "@/configs/translation.config";
 
 const MobileComp = dynamic(() => import("./components/mobile"));
 const DesktopComp = dynamic(() => import("./components/desktop/"));
 
 
 export default async function NavBar() {
-    const t = await getTranslations("Headings");
+    const t = await getTranslations(Namespaces.headings);
 
     function translateNavigationHeadings(data: NavBarHeading[]) {
         return data.map(item => ({
