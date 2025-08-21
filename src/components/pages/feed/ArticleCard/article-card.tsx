41314a9865a7b8e12c7c3982f671c87a59245f5a
Dmytro <dmytropolizhai@gmail.com>
2025-08-21 04:36:52 +0300

    fix(url): relative url from admin was fixed

--- src/components/pages/feed/ArticleCard/article-card.tsx
+++ src/components/pages/feed/ArticleCard/article-card.tsx
@@ -3,7 +3,7 @@ import { ArticleProps } from "./article-card.types";
 
 import Text from "@/components/ui/Text";
 import Link from "next/link";
-import { getHeadingUrl } from "@/configs/url.config";
+import { getAdmin, getHeadingUrl } from "@/configs/url.config";
 
 const ArticleCard = ({
                   title,
@@ -24,7 +24,7 @@ const ArticleCard = ({
             className={styles.post}
             style={{
                 background: `linear-gradient(0deg, rgba(0, 0, 0, 0.20) 0%, rgba(0, 0, 0, 0.20) 100%), 
-                url(${cover.url}) lightgray 50% / cover no-repeat`,
+                url(${getAdmin()(cover.url)}) lightgray 50% / cover no-repeat`,
             }}
             tabIndex={0}
             aria-label={`Read full article: ${title}`}
