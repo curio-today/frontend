7989d8a88bcfd6627f67063d99f75e10f604d5fa
Dmytro <dmytropolizhai@gmail.com>
2025-08-23 10:30:48 +0300

    fix(converter): changed node unknown to SerializedBlockNode

--- src/helpers/converters.tsx
+++ src/helpers/converters.tsx
@@ -1,11 +1,11 @@
 import { JSXConvertersFunction } from '@payloadcms/richtext-lexical/react';
 import QuoteBlock from "@/components/blocks/QuoteBlock";
+import { SerializedBlockNode } from "@payloadcms/richtext-lexical";
 
 export const blockConverters: JSXConvertersFunction = ({ defaultConverters }) => ({
     ...defaultConverters,
     blocks: {
-        quote: ({ node }: { node: unknown }) => {
-
+        quote: ({ node }: { node: SerializedBlockNode }) => {
             return <QuoteBlock text={node.fields.text} author={node.fields.author}/>
         },
     },
