11ee430e4ab8a2d06f60a1b6b51412efbb1bdccf
Dmytro <dmytropolizhai@gmail.com>
2025-08-23 10:05:33 +0300

    fix(config): changed output to standalone

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
+    output: "standalone",
     images: {
         remotePatterns: [new URL('https://admin.curio.today/**')]
     }
