6f45cd33072971a14d86299b0647c40385504762
Dmytro <dmytropolizhai@gmail.com>
2025-08-25 22:50:43 +0300

    feat(nav-bar): desktop nav bar changed to blur background

--- src/components/navigation/NavBar/components/desktop/desktop.tsx
+++ src/components/navigation/NavBar/components/desktop/desktop.tsx
@@ -4,18 +4,19 @@ import styles from "./desktop.module.css"
 import dynamic from "next/dynamic";
 import Links from "./components/links";
 import { NavBarProps } from "@/components/navigation/NavBar";
+import { useEffect, useState } from "react";
+import clsx from "clsx";
 
 const CurioLogo = dynamic(() => import("@/components/branding/CurioLogo"), { ssr: false });
 const Buttons = dynamic(() => import("./components/buttons"));
 
 const Desktop = ({ headings }: NavBarProps) => {
-
     return (
-        <header className={styles.navBar}>
+        <nav className={styles.navBar}>
             <CurioLogo />
             <Links headings={headings}/>
             <Buttons />
-        </header>
+        </nav>
     )
 }
 
