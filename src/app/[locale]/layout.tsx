43cb5e0251e2fa27c1aef2ada33f68e634c56146
Dmytro <dmytropolizhai@gmail.com>
2025-09-01 20:52:12 +0300

    fix(language-html): now in html tag it has a language

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -28,9 +28,6 @@ export default async function RootLayout({
 }) {
     const { locale } = await params;
 
-    if (!hasLocale(routing.locales, locale)) {
-    }
-
     return (
         <html lang={locale} className={outfit.className}>
             <body>
