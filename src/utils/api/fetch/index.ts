9f47f021692c24e6bc9a56fd8f447b6ebd74d550
Dmytro <dmytropolizhai@gmail.com>
2025-09-16 21:56:49 +0300

    fix(index.ts): add "beautiful" import from fetch directory

---
+++ src/utils/api/fetch/index.ts
@@ -0,0 +1,5 @@
+export { fetchArticleBySlug } from "./fetch-article-by-slug";
+export { fetchArticleById } from "./fetch-article-by-id";
+export { fetchArticle } from "./fetch-article";
+export { fetchEndpointList } from "./fetch-endpoint-list" 
+export { fetchEndpointDetail } from "./fetch-endpoint-detail" 
