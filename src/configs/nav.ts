db5cf186b3839ac40c81add99696820e8c216111
Dmytro Polizhai <dmytropolizhai@gmail.com>
2025-09-17 10:43:03 +0300

    feat(nav): move data from nav.data.ts to nav.ts

--- src/configs/nav.ts
+++ src/configs/nav.ts
@@ -1,24 +1,53 @@
 import { NavigationConfig } from "@/types/configs";
 
-// export const Navigation: NavigationConfig = {
-//     desktop: {
-//         items: [
-//             {
-//                 key: "example",
-//                 href: "/example",
-//                 icon: "example-icon-name",
-//             },
-//             // ...
-//         ]
-//     },
-//     mobile: {
-//         items: [
-//             {
-//                 key: "example",
-//                 href: "/example",
-//                 icon: "example-icon-name",
-//             },
-//             // ...
-//         ]
-//     }
-// }
+export const Navigation: NavigationConfig = {
+    desktop: {
+        items: [
+            {
+                key: "amazes",
+                href: "/feed/amazes",
+            },
+            {
+                key: "informs",
+                href: "/feed/informs",
+            },
+            {
+                key: "amuses",
+                href: "/feed/amuses",
+            },
+            {
+                key: "inspires",
+                href: "/feed/inspires"
+            }
+        ]
+    },
+    mobile: {
+        items: [
+            {
+                key: "amazes",
+                href: "/feed/amazes",
+                icon: "wow",
+            },
+            {
+                key: "informs",
+                href: "/feed/informs",
+                icon: "megaphone",
+            },
+            {
+                key: "feed",
+                href: "/feed",
+                icon: "newspaper"
+            },
+            {
+                key: "amuses",
+                href: "/feed/amuses",
+                icon: "question_mark",
+            },
+            {
+                key: "inspires",
+                href: "/feed/inspires",
+                icon: "lightbulb"
+            }
+        ]
+    }
+}
