226a73e94b103b501bbff7e400747650b31d0019
Dmytro Polizhai <dmytropolizhai@gmail.com>
2025-09-17 11:26:13 +0300

    fix(nav-config): add jsdoc to navigation type

--- src/types/configs/nav.ts
+++ src/types/configs/nav.ts
@@ -1,8 +1,17 @@
 import { FeedItem } from "../pages/feed-item";
 import { Platform } from "../platform";
 
+/**
+ * Represents the configuration for navigation items per platform.
+ *
+ * @typedef {Object} NavigationConfig
+ * @property {Platform} - A key representing each platform (mobile or desktop).
+ * @property {Array<FeedItem>} [platform].items - An array of feed items for the platform.
+ *
+ * @template Platform - A union type representing supported platform names (e.g., 'web', 'mobile').
+ */
 export type NavigationConfig = {
     [platform in Platform]: {
-        items: Array<FeedItem>;
+        items: FeedItem[];
     }
 }
