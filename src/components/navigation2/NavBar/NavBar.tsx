3a04a3a5df67dd8c86e5b95e1754d38896020a4d
Dmytro Polizhai <dmytropolizhai@gmail.com>
2025-09-18 11:59:38 +0300

    feat(nav-bar): create props fo rNavigationBar

--- src/components/navigation2/NavBar/NavBar.tsx
+++ src/components/navigation2/NavBar/NavBar.tsx
@@ -1,20 +1,23 @@
 "use server"
 
+import { AvailableRoutePath } from "@/types/navigation";
 import { isMobile } from "@/utils/isMobile";
 import dynamic from "next/dynamic"
 
 export type NavigationBarProps = {
-
+    headers: AvailableRoutePath[]
 }
 
 const MobileNavigationBar = dynamic(() => import("./mobile/"));
 const DesktopNavigationBar = dynamic(() => import("./desktop/"));
 
 export default async function NavigationBar() {
+    const headers: NavigationBarProps["headers"] = ["amazes", "amuses", "informs", "inspires"] 
+    
     if (await isMobile()) {
-        return <MobileNavigationBar />
+        return <MobileNavigationBar headers={headers} />
     }   
 
-    return <DesktopNavigationBar />
+    return <DesktopNavigationBar headers={headers} />
 }
 
