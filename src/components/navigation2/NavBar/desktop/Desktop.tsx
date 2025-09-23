e8e6f5fc7894f72456e4ff2d046725103281902d
Dmytro Polizhai <dmytropolizhai@gmail.com>
2025-09-23 15:51:40 +0300

    fix(logo): add CurioLogo to the navigation bar

--- src/components/navigation2/NavBar/desktop/Desktop.tsx
+++ src/components/navigation2/NavBar/desktop/Desktop.tsx
@@ -1,16 +1,17 @@
-"use client"
+"use client";
 
 import { useNavigationBar } from "@/hooks/useNavigationBar";
 import styles from "./Desktop.module.css";
 
 import { NavigationBarActionButtons, NavigationBarLinks } from "./components";
+import CurioLogo from "@/components/branding/CurioLogo";
 
-
-const DesktopNavigationBar = () => {  
-  const { navigationLinks } = useNavigationBar()
+const DesktopNavigationBar = () => {
+  const { navigationLinks } = useNavigationBar();
 
   return (
     <nav className={styles.navBar}>
+      <CurioLogo />
       <NavigationBarLinks navigationLinks={navigationLinks} />
       <NavigationBarActionButtons />
     </nav>
