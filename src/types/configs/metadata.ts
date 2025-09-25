54c91586525e6b6b3a4ad3f09566deeb9fb367c6
Dmytro Polizhai <dmytropolizhai@gmail.com>
2025-09-25 11:30:54 +0300

    feat(metadata-config): init logic

--- src/types/configs/metadata.ts
+++ src/types/configs/metadata.ts
@@ -1,5 +1,14 @@
+import { Metadata } from "next";
+import { AvailableRoutePath } from "../navigation";
+
 export type MetadataConfig = {
-    siteName: string;
+    prefix: string,
+    siteName: string,
+    delimiter: string,
+
+    static: Partial<{
+        [route in AvailableRoutePath]: Metadata        
+    }>
 }
 
 export default MetadataConfig;
