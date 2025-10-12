b34f59db92e6725f011602bf6635f944bc2082a9
Dmytro <dmytropolizhai@gmail.com>
2025-10-12 08:11:43 +0300

    feat(get-page-metadata-with-translations): add example in jsdoc

--- src/utils/get-page-metadata-with-translation.ts
+++ src/utils/get-page-metadata-with-translation.ts
@@ -1,11 +1,15 @@
 import { getLocale, getTranslations } from "next-intl/server";
 import { getPageMetadata } from "./metadata/get-page-metadata";
 import { AvailableRoutePath } from "@/types/navigation";
+import { Metadata } from "next";
 
 /**
  * Return page metadata with translation using getTranslations function
  * @param route {AvailableRoutePath} website route to get metadata
- * @returns 
+ * @example
+ * export async function generateMetadata(): Promise<Metadata> {
+     return getPageMetadataWithTranslation("feed"); // feed -> AvailableRoutePath
+ * }
  */
 export async function getPageMetadataWithTranslation(route: AvailableRoutePath): Promise<ReturnType<typeof getPageMetadata> | null> {
     const locale = await getLocale();
