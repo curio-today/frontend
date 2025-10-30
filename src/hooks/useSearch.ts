e16950d41b19ccb6e1aec1c85b0ba9d2d0ece06d
Dmytro <dmytropolizhai@gmail.com>
2025-10-30 18:28:35 +0400

    feat(use-search): Now useSearch hook is generic!
    Arguments is changed to a full type. In args added a stringify function to make search easier for custom types.

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-type SearchResult = {
-    filteredData: string[],
+type SearchResult<TSearchItem> = {
+    filteredData: TSearchItem[],
     lastTerm: string | null,
 
-    search: (term: string) => string[],
+    search: (term: string) => TSearchItem[],
     clear: () => void,
 }
 
@@ -12,15 +12,16 @@ type SearchResult = {
  * Hook to search in fields by using function search()
  * @returns 
  */
-export const useSearch = (data: string[], isDefault?: boolean): SearchResult => {
-    const [filteredData, setFilteredData] = useState<string[]>(isDefault ? data : []);
+export const useSearch = <TSearchItem>(data: TSearchItem[], filter: (term: string, item: TSearchItem) => boolean,  isDefault: boolean = false): SearchResult<TSearchItem> => {
+    const [filteredData, setFilteredData] = useState<TSearchItem[]>(isDefault ? data : []);
 
     let lastTerm: string | null = null;
 
     return {
         search: (term) => {
-            const newFilteredData = data.filter(item => item.toLowerCase().includes(term.toLowerCase()));
-            
+            // Use custom filter from args
+            const newFilteredData = data.filter(item => filter(term, item));
+
             // Prevents re-render if new data is equal to old data
             if (newFilteredData != filteredData) {
                 setFilteredData(newFilteredData);
