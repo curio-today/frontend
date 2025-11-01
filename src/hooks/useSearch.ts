9be19880ad1e375d892c2ef1770c57a360120e06
Dmytro <dmytropolizhai@gmail.com>
2025-11-01 12:15:31 +0400

    feat(use-search): Mark as deprecated

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,5 +1,8 @@
 import { useState } from "react";
 
+/**
+ * @deprecated
+ */
 export type SearchResult<TSearchItem> = {
     filteredData: TSearchItem[],
     searchTerm: string,
@@ -8,8 +11,12 @@ export type SearchResult<TSearchItem> = {
     clear: () => void,
 }
 
+/**
+ * @deprecated
+ */
 export type SearchArguments<TSearchItem> = {
     data: TSearchItem[]; 
+    loadMoreData: (term: string) => Promise<TSearchItem[]>;
     searchBy: (item: TSearchItem) => string;
     isDefault?: boolean;
 }
@@ -20,8 +27,9 @@ export type SearchArguments<TSearchItem> = {
  * @param searchBy - a function that returns a string to make search happen to custom types.
  * @param isDefault - a flag to make a filtered data by default set to data in args
  * @returns {SearchResult}
+ * @deprecated
  */
-export const useSearch = <TSearchItem, >({ data, searchBy, isDefault }: SearchArguments<TSearchItem>): SearchResult<TSearchItem> => {
+export const useSearch = <TSearchItem, >({ data, loadMoreData, searchBy, isDefault }: SearchArguments<TSearchItem>): SearchResult<TSearchItem> => {
     const [filteredData, setFilteredData] = useState<TSearchItem[]>(isDefault ? data : []);
 
     let searchTerm: string = "";
@@ -29,16 +37,20 @@ export const useSearch = <TSearchItem, >({ data, searchBy, isDefault }: SearchAr
     return {
         search: (term) => {
             // Use custom filter from args
-            const newFilteredData = data.filter(item => searchBy(item).toLowerCase().includes(term.toLowerCase()));
+            loadMoreData(term).then(newData => {
+                const newFilteredData = newData.filter(item => searchBy(item).toLowerCase().includes(term.toLowerCase()));
 
-            // Prevents re-render if new data is equal to old data
-            if (newFilteredData != filteredData) {
-                setFilteredData(newFilteredData);
-            }
-            
+                // Prevents re-render if new data is equal to old data
+                if (newFilteredData != filteredData) {
+                    setFilteredData(newFilteredData);
+                }
+            }).catch(err => {
+                console.log(err);
+            });
+
             searchTerm = term;            
             
-            return newFilteredData;
+            return filteredData;
         },
         clear: () => {
             setFilteredData([]);
