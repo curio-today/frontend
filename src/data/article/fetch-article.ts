9ca60c8796de180ca5c750ab19802c8b20054e05
Dmytro <dmytropolizhai@gmail.com>
2025-12-03 23:37:27 +0200

    fix: fetch-article buildUrl

--- src/data/article/fetch-article.ts
+++ src/data/article/fetch-article.ts
@@ -1,12 +1,11 @@
 import { Article } from "@/types/api/article";
 import { fetchWithCache } from "@/lib/fetch-with-cache";
 import { buildUrl } from "@/lib/build-url";
-import { PaginatedContent } from "@/types/api/paginated-article";
 
 import { QueryParams } from "next-intl/navigation";
 
-export async function fetchArticle(id: string, query?: QueryParams): Promise<PaginatedContent<Article>> {
-    const url =  buildUrl(`/post/${id}`, query)
+export async function fetchArticle(id: string, query?: QueryParams): Promise<Article> {
+    const url =  buildUrl(`/posts/${id}`, query)
 
     try {
         const response = await fetchWithCache(url);
@@ -15,9 +14,9 @@ export async function fetchArticle(id: string, query?: QueryParams): Promise<Pag
             throw new Error(`Failed to fetch admin url: ${url}`);
         }
 
-        return await response.json() as Promise<PaginatedContent<Article>>;
+        return await response.json() as Promise<Article>;
     } 
     catch (err) {
-        throw new Error("Unexpected error occured. Try again later");
+        throw new Error(`${err}\nUnexpected error occured. Try again later`);
     }
 }
