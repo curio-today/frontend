c516d62c4b831b4f1885fc7ff7553ecc8cd4114f
Dmytro <dmytropolizhai@gmail.com>
2025-12-03 18:58:35 +0200

    feat(category): add Category type and add CATEGORY_PATHNAME_MAP

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -1,7 +1,9 @@
+import { Category } from "@/types/category";
+
 /**
  * Constant to save a categories
  */
-export const CATEGORY_LIST = ["amazes", "informs", "amuses", "inspires"] as const;
+export const CATEGORY_LIST = ["home", "amazes", "informs", "amuses", "inspires"] as const;
 
 /**
  * Because our backend returns a id of category, we have to check it manually to create human-friendly url. Needs to be fix on the backend
@@ -11,4 +13,12 @@ export const CATEGORY_ID_SLUG_MAP: Record<string, string> = {
     "68a06acca7da60cbf9116d42": "informs",
     "68a42704dff1cb31b03d40f3": "amazes",
     "68a42852dff1cb31b03d4371": "amuses",
+}
+
+export const CATEGORY_PATHNAME_MAP: Record<string, Category> = {
+    "/amazes": "amazes",
+    "/amuses": "amuses",
+    "/inspires": "inspires",
+    "/informs": "informs",
+    "/": "home"
 }
