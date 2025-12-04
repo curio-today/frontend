93c7cf725d17a9b28bcd30b74ef1b04d84c549a2
Dmytro <dmytropolizhai@gmail.com>
2025-12-04 16:53:28 +0200

    fix(article-loading): fix styles

--- src/app/[locale]/[category]/[id]/loading.tsx
+++ src/app/[locale]/[category]/[id]/loading.tsx
@@ -4,13 +4,13 @@ import { Skeleton } from "@/components/core/skeleton";
 export default function Loading() {
     return (
         <section className="article flex flex-col gap-12 w-full h-full">
-            <Skeleton className="w-full h-10 flex flex-row gap-4 align-middle justify-start text-center" />
+            <Skeleton className="w-full h-10" />
             <div className="container flex flex-col gap-4">
-                <Skeleton className="w-full h-40 flex flex-row gap-4 align-middle justify-start text-center" />
-                <Skeleton className="w-full h-10 flex flex-row gap-4 align-middle justify-start text-center" />
+                <Skeleton className="w-full h-40" />
+                <Skeleton className="w-full h-10 " />
             </div>
             <Separator />
-            <Skeleton className="w-full h-200 flex flex-row gap-4 align-middle justify-start text-center" />
+            <Skeleton className="w-full h-200" />
         </section>
     )
 }
