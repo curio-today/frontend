1b11df403c0be1bed6c5d698809b4b9ef073245e
Dmytro <dmytropolizhai@gmail.com>
2025-12-06 20:17:58 +0200

    fix(time): fix tabulation

--- src/components/ui/time.tsx
+++ src/components/ui/time.tsx
@@ -1,12 +1,12 @@
 import { Locale } from "next-intl"
 import { ComponentProps } from "react"
 
-    function formateDate(iso: string, locale: Locale): string {
-        return new Intl.DateTimeFormat(locale, {
-            dateStyle: "full",
-            timeStyle: "short"
-        }).format(new Date(iso)).toUpperCase()
-    }
+function formateDate(iso: string, locale: Locale): string {
+    return new Intl.DateTimeFormat(locale, {
+        dateStyle: "full",
+        timeStyle: "short"
+    }).format(new Date(iso)).toUpperCase()
+}
 
 export const Time = ({ locale, iso, ...props }: { locale: Locale, iso: string } & Omit<ComponentProps<"time">, "children">) => {
     return (
