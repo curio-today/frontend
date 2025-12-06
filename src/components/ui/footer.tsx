c214914408e0826e99075c6fc5af482d445a2a92
Dmytro <dmytropolizhai@gmail.com>
2025-12-07 00:09:40 +0200

    feat(footer): add "All rights are reserved"

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -6,6 +6,7 @@ export const Footer = () => {
         <footer className="w-full h-50 bg-background flex justify-center items-center flex-col">
             <Logo width={100}/>
             <Memo />
+            <p className="text-muted-foreground">All Rights Are Reserved©</p>
         </footer>  
     ) 
 }
