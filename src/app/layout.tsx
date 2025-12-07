4cb1d7ace3d3d2804c0d5457add0f5fa633633cc
Dmytro <dmytropolizhai@gmail.com>
2025-12-07 21:13:37 +0200

    feat(analytics): add Google Tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,8 @@ import LayoutProvider from "@/providers/layout-provider";
 import { QueryProvider } from "@/providers/query-provider";
 import { Toaster } from "@/components/core/sonner";
 import { Separator } from "@/components/core/separator";
+import Head from "next/head";
+import Script from "next/script";
 
 const outfit = Roboto({
     subsets: ["latin"],
@@ -30,7 +32,26 @@ export default async function RootLayout({ children }: Readonly<PropsWithChildre
 
     return (
         <html lang={locale} className={outfit.className} suppressHydrationWarning>
+            <Head>
+                <Script id="gtm-init" strategy="afterInteractive">
+                {`
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-WWZZ33KD');
+                `}
+                </Script>
+            </Head>
             <body>
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-WWZZ33KD"
+                        height="0"
+                        width="0"
+                        className="hidden"
+                    ></iframe>
+                </noscript>
                 <QueryProvider>
                     <NextIntlClientProvider>
                         <ThemeProvider 
