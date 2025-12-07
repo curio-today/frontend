a74834a661d2d3544fad53b8afc0a4bdf8921df2
Dmytro <dmytropolizhai@gmail.com>
2025-12-07 22:34:44 +0200

    feat(analytics): add Google Analytics scripts to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,19 @@ export default async function RootLayout({ children }: Readonly<PropsWithChildre
     return (
         <html lang={locale} className={outfit.className} suppressHydrationWarning>
             <body>
+                {/* Google Analytics */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-MNV4816TQ0"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-MNV4816TQ0');
+                `}
+                </Script>
                 <QueryProvider>
                     <NextIntlClientProvider>
                         <ThemeProvider 
