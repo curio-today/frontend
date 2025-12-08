ec986c670ce1008b2ace6f92f682f60c1fb21c10
Dmytro <dmytropolizhai@gmail.com>
2025-12-08 16:34:11 +0200

    fix(memo): centralized memo

--- src/components/ui/memo.tsx
+++ src/components/ui/memo.tsx
@@ -6,6 +6,8 @@ export async function Memo() {
     const t = await getTranslations("Messages");
 
     return (
-        <small className="uppercase max-w-25 justify-center text-center">{t("memo")}</small>
+        <small className="flex items-center justify-center text-center uppercase max-w-25">
+            {t("memo")}
+        </small>
     )
 }
