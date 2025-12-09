e6577c64eb3960348590a9984d9a575b0b0edd9f
Dmytro <dmytropolizhai@gmail.com>
2025-12-09 23:10:22 +0200

    feat(locale): now when changing locale client won't be send to main page

--- src/hooks/use-change-locale.ts
+++ src/hooks/use-change-locale.ts
@@ -1,12 +1,15 @@
+import { usePathname } from "@/i18n/navigation";
 import { Locale, useLocale } from "next-intl";
 import { useRouter } from "next/navigation";
 
 export const useChangeLocale = () => {
     const locale = useLocale();
     const router = useRouter();
+    const pathname = usePathname();
+
     const changeLocale = (newLocale: Locale) => {
         if (newLocale !== locale) {
-            router.replace(`/${newLocale}`, { scroll: false, })
+            router.replace(`/${newLocale}${pathname}`, { scroll: false, })
             router.refresh();
         }
     };
