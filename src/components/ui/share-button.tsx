716fc6bd91d54d968074dd409e638075ca3b8bb8
Dmytro <dmytropolizhai@gmail.com>
2025-12-09 16:28:33 +0200

    feat(share-button): add copy button to copy link

--- src/components/ui/share-button.tsx
+++ src/components/ui/share-button.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { usePathname } from "@/i18n/navigation"
 import { Button } from "../core/button"
 import { 
     Dialog, 
@@ -12,10 +11,17 @@ import {
     DialogClose,
     DialogFooter
 } from "../core/dialog"
-import { Input } from "../core/input"
+import { 
+    Tooltip, 
+    TooltipTrigger,
+    TooltipContent
+} from "../core/tooltip"
 import { Label } from "../core/label"
-import { Share } from "lucide-react"
+import { Copy, Share } from "lucide-react"
 import { useTranslations } from "next-intl"
+import { InputGroup, InputGroupButton, InputGroupInput } from "../core/input-group"
+import { usePathname } from "@/i18n/navigation"
+import { toast } from "sonner"
 
 
 
@@ -23,10 +29,11 @@ export const ShareButton = () => {
     const pathname = usePathname();
     const t = useTranslations("Buttons.Share");
 
-    const link = `https://curio.today${pathname}`
+    const link = "https://curio.today" + pathname;
 
     const copyLink = () => {
-
+        navigator.clipboard.writeText(link);
+        toast.success(t("success"))
     }
 
     return (
@@ -47,16 +54,30 @@ export const ShareButton = () => {
                 <div className="flex items-center gap-2">
                     <div className="grid flex-1 gap-2">
                         <Label htmlFor="link" className="sr-only">{t("sr")}</Label>
-                        <Input
-                            id="link"
-                            defaultValue={link}
-                            readOnly
-                        />
+                        <InputGroup>
+                            <InputGroupInput
+                                id="link"
+                                defaultValue={link}
+                                readOnly
+                            >
+                            </InputGroupInput>
+                            <Tooltip>
+                                <TooltipTrigger asChild>
+                                    <InputGroupButton onClick={copyLink}>
+                                        <Copy />
+                                    </InputGroupButton>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>Copy link to this article</p>
+                                </TooltipContent>
+                            </Tooltip>
+
+                        </InputGroup>
                     </div>
                 </div>
                 <DialogFooter className="sm:justify-start">
                     <DialogClose asChild>
-                        <Button type="button" variant="secondary">{t("buttons.close")}</Button>
+                        <Button variant="secondary">{t("buttons.close")}</Button>
                     </DialogClose>
                 </DialogFooter>
             </DialogContent>
