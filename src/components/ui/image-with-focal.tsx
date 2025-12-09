e9307d8184cef877911d256869383a658cabcb1a
Dmytro <dmytropolizhai@gmail.com>
2025-12-09 19:35:49 +0200

    fix(image-with-focal): fix focal points

--- src/components/ui/image-with-focal.tsx
+++ src/components/ui/image-with-focal.tsx
@@ -2,7 +2,7 @@ import Image, { ImageProps } from "next/image";
 
 export const ImageWithFocal = ({ focalX, focalY, children, alt, ...rest }: ImageProps & { focalX: number, focalY: number }) => {
     return (
-        <Image style={{ objectPosition: `${focalX} ${focalY}`, objectFit: "cover"}} alt={alt}  {...rest}>
+        <Image style={{ objectPosition: `${focalX}% ${focalY}%`, objectFit: "cover"}} alt={alt}  {...rest}>
             {children}
         </Image>
     )
