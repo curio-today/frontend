2793b7fb59295eb3edbb190cb076ba1b6932d0f4
Dmytro <dmytropolizhai@gmail.com>
2025-12-10 20:26:34 +0200

    refactor(time): remove "locale" in props, instead useLocale()

--- src/components/ui/time.tsx
+++ src/components/ui/time.tsx
@@ -1,4 +1,6 @@
-import { Locale } from "next-intl"
+"use client"
+
+import { Locale, useLocale } from "next-intl"
 import { ComponentProps } from "react"
 
 function formateDate(iso: string, locale: Locale): string {
@@ -8,7 +10,9 @@ function formateDate(iso: string, locale: Locale): string {
     }).format(new Date(iso)).toUpperCase()
 }
 
-export const Time = ({ locale, iso, ...props }: { locale: Locale, iso: string } & Omit<ComponentProps<"time">, "children">) => {
+export const Time = ({ iso, ...props }: { iso: string } & Omit<ComponentProps<"time">, "children">) => {
+    const locale = useLocale();
+    
     return (
         <time {...props}>{formateDate(iso, locale)}</time>
     )
