b27932515f9f25833d951338215c4bc4d38b5204
Dmytro <dmytropolizhai@gmail.com>
2025-12-10 20:10:36 +0200

    refactor(category-badge): rename to ArticleBadge

--- src/components/ui/article/article-badge.tsx
+++ src/components/ui/article/article-badge.tsx
@@ -3,12 +3,18 @@ import { CATEGORY_ID_SLUG_MAP } from "@/constants/categories";
 import { Article } from "@/types/api/article";
 import { BadgeInfoIcon } from "lucide-react";
 import Link from "next/link";
+import { ComponentProps } from "react";
 
-export const CategoryBadge = ({ badge }: { badge: Article["badge"] }) => (
+export type ArticleBadgeProps = {
+    badge: Article["badge"],
+} & ComponentProps<"span">
+
+export const ArticleBadge = ({ badge, ...rest }: ArticleBadgeProps) => (
     <Link href={`/${CATEGORY_ID_SLUG_MAP[badge.id]}`}>
         <Badge 
             className="bg-blue-400"
             variant="secondary"
+            {...rest}
         >
             <BadgeInfoIcon />
             {badge.name}
