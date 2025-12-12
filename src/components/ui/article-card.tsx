7e44be3d06b739712fa33aededfb552206fa8753
Dmytro <dmytropolizhai@gmail.com>
2025-12-12 16:14:27 +0200

    feat(article-card): add suspenses

--- src/components/ui/article-card.tsx
+++ src/components/ui/article-card.tsx
@@ -12,30 +12,37 @@ import { Article } from "@/types/api/article"
 import { ImageWithFocal } from "./image-with-focal"
 import { Time } from "./time"
 import { ArticleBadge } from "./article"
+import { Suspense } from "react"
 
 export type ArticleCardProps = Pick<Article, "title" | "cover" | "slug" | "subtitle" | "id" | "badge" | "createdAt" | "badge">
 
 export function ArticleCard({ title, subtitle, slug, cover, createdAt, badge }: ArticleCardProps) {
   return (
-    <Card className="gap-5 flex-1 h-full hover:outline-1 hover:outline-blue-400" id={slug}>
+    <Card className="gap-5 flex-1 h-full hover:outline-1 hover:outline-primary" id={slug}>
       <CardHeader>
         <CardTitle className="text-xl line-clamp-3 min-h-15">{title}</CardTitle>
-        <CardContent className="relative aspect-video overflow-hidden rounded-xl w-full">
-          <ImageWithFocal src={cover.url} alt={cover.alt} focalX={cover.focalX} focalY={cover.focalY} fill />
-        </CardContent>
+        <Suspense fallback={<Skeleton className="w-100"/>}>
+          <CardContent className="relative aspect-video overflow-hidden rounded-xl w-full">
+            <ImageWithFocal src={cover.url} alt={cover.alt} focalX={cover.focalX} focalY={cover.focalY} fill />
+          </CardContent>
+        </Suspense>
       </CardHeader>
       <CardFooter className="flex-col items-start gap-2">
-        <CardDescription className="line-clamp-2 text-left font-medium">{subtitle}</CardDescription>
+        <CardDescription className="line-clamp-2 text-left font-medium">
+          {subtitle}
+        </CardDescription>
         <div className="flex flex-row w-full items-center">
           <Time 
-              className="flex-1 text-muted-foreground font-thin text-xs align-middle text-left" 
-              iso={createdAt}
+            className="flex-1 text-muted-foreground font-thin text-xs align-middle text-left" 
+            iso={createdAt}
           />
-          <ArticleBadge 
-            name={badge.name}
-            id={badge.id}
-            variant="outline"
-          />
+          <Suspense fallback={<Skeleton className="w-50 h-50"/>}>
+            <ArticleBadge 
+              name={badge.name}
+              id={badge.id}
+              variant="outline"
+            />
+          </Suspense>
         </div>
       </CardFooter>
     </Card>
