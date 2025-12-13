91b2a527160ea6dcc92e78749f2f45365193b795
Dmytro <dmytropolizhai@gmail.com>
2025-12-13 14:23:17 +0200

    fix(build-url): add alternative to NEXT_PUBLIC_API_URL

--- src/lib/build-url.ts
+++ src/lib/build-url.ts
@@ -2,7 +2,7 @@ import { QueryParams } from "next-intl/navigation";
 import { stringify } from "querystring";
 
 export function buildUrl(endpoint: string, query?: QueryParams): URL {
-    const base = process.env.NEXT_PUBLIC_API_URL;
+    const base = process.env.NEXT_PUBLIC_API_URL || "https://admin.curio.today/api";
 
     const url = base + endpoint + (query ? `?${stringify(query)}` : "")
 
