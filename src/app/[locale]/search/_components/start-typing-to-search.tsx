597ae9086357c444f3263b3710dfc60102507324
Dmytro <dmytropolizhai@gmail.com>
2025-12-13 13:43:09 +0200

    add StartTypingToSearch

---
+++ src/app/[locale]/search/_components/start-typing-to-search.tsx
@@ -0,0 +1,29 @@
+import { 
+    Empty, 
+    EmptyDescription, 
+    EmptyHeader,
+    EmptyMedia,
+    EmptyTitle
+} from "@/components/core/empty"
+import { Search } from "lucide-react"
+import { getTranslations } from "next-intl/server"
+
+export const StartTypingToSearch = async () => {
+    const t = await getTranslations("Empties.startTypingToSearch");
+
+    return (
+        <Empty>
+            <EmptyHeader>
+                <EmptyMedia variant="icon">
+                    <Search />
+                </EmptyMedia>
+                <EmptyTitle>
+                    {t("title")}
+                </EmptyTitle>
+                <EmptyDescription>
+                    {t("description")}
+                </EmptyDescription>
+            </EmptyHeader>
+        </Empty>
+    )
+}
