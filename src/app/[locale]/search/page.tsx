6fa4e960defd0bf5d7a7b8a00b117cf50f17aaea
Dmytro <dmytropolizhai@gmail.com>
2025-12-13 12:58:51 +0200

    create SearchPage

---
+++ src/app/[locale]/search/page.tsx
@@ -0,0 +1,30 @@
+"use server";
+
+import { FeaturedArticlesGrid } from "@/components/ui/featured-articles-grid";
+import { SearchForm } from "@/components/ui/search-form";
+import { getArticlesByQuery } from "@/data/article/get-articles-by-query";
+import { getMetadata } from "@/data/metadata/get-metadata";
+import { getLocale } from "next-intl/server";
+
+export async function generateMetadata() {
+    return getMetadata("search");
+}
+
+export default async function SearchPage({ searchParams }: { searchParams: Promise<{ q: string | null }> }) {
+    const { q } = await searchParams;
+    const locale = await getLocale();
+    const { docs: articles } = await getArticlesByQuery(q || "", { locale });
+
+    return (
+        <>
+            <div className="flex gap-4 flex-col w-full">
+                <SearchForm initQuery={q || ""}/>
+                {articles.length === 0 ? (
+                    <p>No articles available 📰</p>
+                ) : (
+                    <FeaturedArticlesGrid articles={articles} />
+                )}
+            </div>
+        </>
+    )
+}
