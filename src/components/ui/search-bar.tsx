a9d1efb739335797dbd81c9d9181388b097e7ee4
Dmytro <dmytropolizhai@gmail.com>
2025-12-16 16:49:04 +0200

    refactor(search-form: remove Form and renamed to SearchBar

---
+++ src/components/ui/search-bar.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { Search } from "lucide-react"
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "../core/input-group"
+import { useTranslations } from "next-intl"
+import { FilterButton } from "./filter-button"
+import { useSearch } from "@/hooks/use-search"
+
+export const SearchBar = () => {
+  const t = useTranslations("Navigation")
+  const { query, setQuery, onKeyDown } = useSearch({ debounceMs: 250 }) // 500ms auto-search
+
+  return (
+    <div className="flex flex-row gap-2 md:gap-4">
+      <InputGroup>
+        <InputGroupAddon>
+          <Search />
+        </InputGroupAddon>
+
+        <InputGroupInput
+          placeholder={t("placeholder")}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={onKeyDown}
+        />
+
+        <InputGroupAddon align="inline-end">
+          <FilterButton />
+        </InputGroupAddon>
+      </InputGroup>
+    </div>
+  )
+}
