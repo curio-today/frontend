1872f27f6dfb7bb3893859f0270a9be20f3b0eb2
Dmytro <dmytropolizhai@gmail.com>
2025-12-17 21:07:07 +0200

    feat(categories): add CATEGORY_SLUG_ID_MAP

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -8,11 +8,18 @@ export const CATEGORY_LIST = ["amazes", "informs", "amuses", "inspires"] as cons
 /**
  * Because our backend returns a id of category, we have to check it manually to create human-friendly url. Needs to be fix on the backend
  */
-export const CATEGORY_ID_SLUG_MAP: Record<string, string> = {
+export const CATEGORY_ID_SLUG_MAP: Record<string, Category> = {
     "68a4285edff1cb31b03d438e": "inspires",
     "68a06acca7da60cbf9116d42": "informs",
     "68a42704dff1cb31b03d40f3": "amazes",
     "68a42852dff1cb31b03d4371": "amuses",
+}
+
+export const CATEGORY_SLUG_ID_MAP: Record<string, string> = {
+    "inspires": "68a4285edff1cb31b03d438e",
+    "informs": "68a06acca7da60cbf9116d42",
+    "amazes": "68a42704dff1cb31b03d40f3",
+    "amuses": "68a42852dff1cb31b03d4371",
 }
 
 export const CATEGORY_PATHNAME_MAP: Record<string, Category> = {
