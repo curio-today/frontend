7afcbea4f356ba2d00c8b0483c6260cb2415ec90
Dmytro <dmytropolizhai@gmail.com>
2025-12-17 21:14:39 +0200

    add FilterButton

---
+++ src/components/ui/filter-button.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import { FilterIcon } from "lucide-react"
+import { 
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuGroup,
+    DropdownMenuSub,
+    DropdownMenuSubTrigger,
+    DropdownMenuPortal,
+    DropdownMenuSubContent,
+    DropdownMenuRadioItem,
+    DropdownMenuRadioGroup
+} from "../core/dropdown-menu"
+import { InputGroupButton } from "../core/input-group"
+import { useTranslations } from "next-intl"
+import { useFilters } from "@/hooks/use-filters"
+import { useCategories } from "@/hooks/use-categories"
+
+
+export const FilterButton = () => {
+    const { filters, setFilter } = useFilters();
+    const { categories } = useCategories();
+    const t = useTranslations("Buttons.Filters");
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <InputGroupButton variant="ghost">
+                    <FilterIcon />
+                </InputGroupButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>{t("label")}</DropdownMenuLabel>
+                <DropdownMenuGroup>
+                    <DropdownMenuSub>
+                        <DropdownMenuSubTrigger>{t("category")}</DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                            <DropdownMenuSubContent>
+                            <DropdownMenuRadioGroup value={filters.category} onValueChange={value => setFilter("category", value)}>
+                                    {categories.map(category => (
+                                        <DropdownMenuRadioItem key={category.key} value={category.key}>
+                                            {category.label}
+                                        </DropdownMenuRadioItem>
+                                    ))}
+                            </DropdownMenuRadioGroup>
+                            </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                </DropdownMenuGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
