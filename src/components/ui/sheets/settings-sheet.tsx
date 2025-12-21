6bad2c094747fa6fb10be0a3023d619661721ae5
Dmytro <dmytropolizhai@gmail.com>
2025-12-21 13:17:19 +0200

    feat(sheets): add isMobile version using Drawer

--- src/components/ui/sheets/settings-sheet.tsx
+++ src/components/ui/sheets/settings-sheet.tsx
@@ -14,9 +14,50 @@ import { useTranslations } from "next-intl"
 import { LanguageToggle } from "../toggles/language-toggle"
 import { ModeToggle } from "../toggles/mode-toggle"
 import { Label } from "../../core/label"
+import { useIsMobile } from "@/hooks/use-mobile"
+import { 
+    Drawer, 
+    DrawerContent, 
+    DrawerDescription, 
+    DrawerFooter, 
+    DrawerHeader, 
+    DrawerTitle, 
+    DrawerTrigger
+} from "@/components/core/drawer"
 
 export const SettingsSheet = () => {
     const t = useTranslations("Settings")
+    const isMobile = useIsMobile();
+
+    if (isMobile) {
+        return (
+            <Drawer>
+                <DrawerTrigger asChild>
+                    <Button variant="outline">
+                        <Settings2 />
+                    </Button>
+                </DrawerTrigger>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>{t("title")}</DrawerTitle>
+                        <DrawerDescription>{t("description")}</DrawerDescription>
+                    </DrawerHeader>
+                    <div className="grid flex-1 auto-rows-min gap-6 px-4">
+                        <div className="grid gap-3">
+                            <Label htmlFor="sheet-demo-name">{t("language")}</Label>
+                            <LanguageToggle />
+                        </div>
+                        <div className="grid gap-3">
+                            <Label htmlFor="sheet-demo-username">{t("mode")}</Label>
+                            <ModeToggle />
+                        </div>
+                    </div>     
+                    <DrawerFooter />
+                </DrawerContent>
+            </Drawer>
+        )
+    }
+
 
     return (
         <Sheet>
