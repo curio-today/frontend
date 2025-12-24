16bfd092ce6956b39de7c760f444aaa2a5218f96
Dmytro <dmytropolizhai@gmail.com>
2025-12-24 11:19:40 +0200

    feat(search-button): add representational text

--- src/components/ui/search-button.tsx
+++ src/components/ui/search-button.tsx
@@ -1,12 +1,18 @@
 import { Search } from "lucide-react"
 import { Button } from "../core/button"
 import Link from "next/link"
+import { getTranslations } from "next-intl/server"
 
-export const SearchButton = () => {
+export const SearchButton = async () => {
+    const t = await getTranslations("Navigation");
+
     return (
         <Button variant="outline" asChild>
             <Link href="/search">
                 <Search />
+                <span className="hidden md:block">
+                    {t("searchButton")}
+                </span>
             </Link>
         </Button>
     )
