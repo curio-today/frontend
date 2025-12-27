3484a00a4300a3042f62f1f578e3845aac9aabe4
Dmytro <dmytropolizhai@gmail.com>
2025-12-27 21:04:21 +0200

    fix(use-suspense-articles): add category translation

--- src/hooks/api/use-suspense-articles.ts
+++ src/hooks/api/use-suspense-articles.ts
@@ -4,14 +4,17 @@ import { getArticles } from "@/data/article/get-articles";
 import { Category } from "@/types/category";
 import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
 import { useLocale } from "next-intl";
+import { useCategoryTranslation } from "../translations/use-category-translation";
 
 export function useSuspenseArticles(category?: Category, options?: Record<string, unknown>) {
     const locale = useLocale();
+    const t = useCategoryTranslation(category ?? "amazes");
+    
     const suspenseQuery = useSuspenseQuery(queryOptions({
         queryKey: ["articles", { locale, category, ...options }],
         queryFn: () => getArticles({
             locale,
-            ...(category ? { "where[badge.name][equals]": category } : {}),
+            ...(category ? { "where[badge.name][equals]": t("title") } : {}),
             ...options
         }),
         staleTime: 5 * 1000,
