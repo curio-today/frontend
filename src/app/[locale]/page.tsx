e2194dea843f425a415ae241d0a967080ffcb93c
Dmytro <dmytropolizhai@gmail.com>
2025-12-27 21:15:33 +0200

    feat(article): add Read Also section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import { ArticlesFeed } from "./_components/articles-feed";
 import { getMetadata } from "@/data/metadata/get-metadata";
-import { ArticlesList } from "./_components/articles-list";
+import { ArticlesList } from "../../components/ui/articles-list";
 import { CATEGORY_LIST } from "@/constants/categories";
 
 export async function generateMetadata() {
