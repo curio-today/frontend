f4183201fc95160e6471d9b1ba51fc737fba6d54
Dmytro <dmytropolizhai@gmail.com>
2026-01-24 10:48:03 +0200

    feat: add Share item to article page

--- src/app/[locale]/[category]/[id]/page.tsx
+++ src/app/[locale]/[category]/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { getArticle } from "@/data/article/get-article";
 import { Separator } from "@/components/ui/separator";
-import { getLocale } from "next-intl/server";
+import { getLocale, getTranslations } from "next-intl/server";
 import { ContentRenderer } from "@/components/feature/content";
 import { Paragraph } from "@/components/typography/paragraph";
 import { ParagraphItalic } from "@/components/typography/paragraph-italic";
@@ -27,6 +27,7 @@ import { ArticlesList } from "@/components/core/articles-list";
 import { CATEGORY_ID_SLUG_MAP } from "@/constants/categories";
 import { Skeleton } from "@/components/ui/skeleton";
 import { ShareHandler } from "@/components/feature/share-handler";
+import { Item, ItemContent, ItemDescription, ItemTitle } from "@/components/ui/item";
 
 const cachedGetArticle = cache(getArticle);
 
@@ -58,6 +59,7 @@ export async function generateMetadata({ params }: { params: Promise<{ id: strin
 export default async function ArticlePage({ params }: { params: Promise<{ id: string }> }) {
     const locale = await getLocale();
     const { id } = await params;
+    const t = await getTranslations("Messages.article");
     const { isEnabled: isDraftModeEnabled } = await draftMode();
     const { title, subtitle, cover, createdAt, source, badge, content } = await getArticle(id, {
         locale,
@@ -66,7 +68,7 @@ export default async function ArticlePage({ params }: { params: Promise<{ id: st
     });
 
     return (
-        <Article className="article flex flex-col gap-8 overflow-hidden">
+        <Article className="flex flex-col gap-8 overflow-hidden">
             <ShareHandler />
             <ArticleMetadata>
                 <Suspense fallback={<Skeleton className="w-100 h-10" />}>
@@ -106,6 +108,22 @@ export default async function ArticlePage({ params }: { params: Promise<{ id: st
                     }}
                 />
             </ArticleContent>
+            <Separator />
+            <ArticleActions>
+                <Item className="flex flex-1 md:flex-col md:justify-start md:items-start justify-center items-center" variant="outline">
+                    <div className="flex flex-col">
+                        <ItemTitle>
+                            {t("share.label")}
+                        </ItemTitle>
+                        <ItemDescription>
+                            {t("share.description")}
+                        </ItemDescription>
+                    </div>
+                    <ItemContent className="w-full">
+                        <ShareButton />
+                    </ItemContent>
+                </Item>
+            </ArticleActions>
             <Separator />
             <Suspense fallback={<Skeleton className="w-full h-200" />}>
                 <ArticleReadAlso>
