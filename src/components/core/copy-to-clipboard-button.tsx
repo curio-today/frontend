877e9adfbb6be2093356130f7cba6356b8aaf55d
Dmytro <dmytropolizhai@gmail.com>
2026-01-24 13:05:28 +0200

    feat(copy-to-clipboard-button): add animations using motion

--- src/components/core/copy-to-clipboard-button.tsx
+++ src/components/core/copy-to-clipboard-button.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Copy } from "lucide-react"
+import { Check, Copy } from "lucide-react"
 import {
     Tooltip,
     TooltipTrigger,
@@ -9,18 +9,33 @@ import {
 import { useTranslations } from "next-intl"
 import { InputGroupButton } from "@/components/ui/input-group"
 import { useClipboard } from "@/hooks/use-clipboard"
-
+import { motion } from "motion/react"
+import { useState } from "react"
 
 
 export const CopyToClipboardButton = ({ text }: { text: string }) => {
     const t = useTranslations("Buttons.Copy");
+    const [isCopied, setIsCopied] = useState<boolean>(false);
     const { copy } = useClipboard();
+
+    function handleCopyClick() {
+        copy(text);
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 1500);
+    }
 
     return (
         <Tooltip>
             <TooltipTrigger asChild>
-                <InputGroupButton onClick={() => copy(text)} variant="ghost">
-                    <Copy />
+                <InputGroupButton variant="ghost" onClick={handleCopyClick}>
+                    <motion.div
+                        key={isCopied ? "check" : "copy"}
+                        initial={{ scale: 0, opacity: 0 }}
+                        animate={{ scale: 1, opacity: 1 }}
+                        exit={{ scale: 0, opacity: 0 }}
+                    >
+                        {isCopied ? <Check /> : <Copy />}
+                    </motion.div>
                 </InputGroupButton>
             </TooltipTrigger>
             <TooltipContent>
