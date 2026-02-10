fed1dcd15a3f47ce7b0cdc6d0bbea5e0ed92f087
Dmytro <dmytropolizhai@gmail.com>
2026-02-10 12:34:13 +0200

    refactor: Add hasVideo function

--- src/app/[locale]/numerocast/page.tsx
+++ src/app/[locale]/numerocast/page.tsx
@@ -10,7 +10,6 @@ import { getNumerocast } from "./_data/get-numerocast";
 import { Suspense } from "react";
 import { getLocale } from "next-intl/server";
 
-
 export default async function NumerocastPage() {
     const t = await getTranslations("numerologgi.numerocast");
     const locale = await getLocale();
@@ -21,6 +20,10 @@ export default async function NumerocastPage() {
 
     const numerocast = await getNumerocast();
 
+    function hasVideo() {
+        return numerocast.video && numerocast.video.src;
+    }
+
     return (
         <>
             <BodyClass className="bg-[#fdfbfd] dark:bg-[#0a0505] overflow-x-hidden selection:bg-[#f88167]/20 transition-colors duration-700" />
@@ -28,20 +31,17 @@ export default async function NumerocastPage() {
 
             <main className="relative z-10 py-0 sm:py-12 max-w-4xl mx-auto space-y-12 sm:space-y-16">
                 <HeaderSection title={t("title")} date={numerocast?.date || today} />
-                {numerocast.video && (
+                {hasVideo() ? (
                     <section className="relative h-dvh sm:h-auto w-screen sm:w-full overflow-hidden sm:rounded-3xl sm:px-6">
                         <ShortVideoPlayer
                             className="h-full w-full sm:aspect-9/16 sm:h-auto sm:max-w-[450px] sm:rounded-3xl rounded-none shadow-none sm:shadow-2xl border-none sm:border border-white/10"
                             src={numerocast?.video?.src || ""}
                             poster={numerocast?.cover?.url || ""}
                             userName="numerologgi"
-                            description={numerocast?.description || "24 ноября..."}
-                            musicName="Original Sound"
                             autoPlay
                         />
                     </section>
-                )}
-                {numerocast?.recommendations && (
+                ) : (
                     <Suspense fallback={<div>Loading...</div>}>
                         <RecommendationList recommendations={numerocast.recommendations} />
                     </Suspense>
