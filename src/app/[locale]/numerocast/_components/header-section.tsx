8e32ee554b7952e2848483245cfb73337ce4c3d2
Dmytro <dmytropolizhai@gmail.com>
2026-02-10 18:17:43 +0200

    fix(header-section): Fix width of the date

--- src/app/[locale]/numerocast/_components/header-section.tsx
+++ src/app/[locale]/numerocast/_components/header-section.tsx
@@ -4,7 +4,7 @@ export const HeaderSection = ({ title, date }: { title: string; date: string }) 
     return (
         <section className="flex flex-row sm:flex-col gap-4 items-center justify-center px-4 sm:px-6">
             <NumerocastTitle />
-            <h2 className="text-lg md:text-xl text-white px-6 py-1.5 bg-[#f88167] w-full rounded-full whitespace-nowrap shadow-lg shadow-[#f88167]/20">
+            <h2 className="text-lg md:text-xl text-white px-6 py-1.5 bg-[#f88167] w-fit rounded-full whitespace-nowrap shadow-lg shadow-[#f88167]/20">
                 {date}
             </h2>
         </section>
