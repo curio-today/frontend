f13d95d6fc2f5926b59af0b108da1cb0a333c3d4
Dmytro <dmytropolizhai@gmail.com>
2026-03-14 11:36:45 +0200

    refactor(numerocast):  Add Promise.all and other little changings

--- src/app/[locale]/numerocast/page.tsx
+++ src/app/[locale]/numerocast/page.tsx
@@ -1,43 +1,53 @@
-import { getTranslations } from "next-intl/server";
+import { getLocale, getTranslations } from "next-intl/server";
 
+import NotFound from "@/app/not-found";
 import { ShortVideoPlayer } from "@/features/video-player";
+
 
 import { HeaderSection } from "./_components/header-section";
 import { RecommendationList } from "./_components/recommendation/recommendation-list";
 import { fetchNumerocast } from "./_data/fetch-numerocast";
-import { getLocale } from "next-intl/server";
-import NotFound from "@/app/not-found";
 
 export default async function NumerocastPage() {
-    const t = await getTranslations("numerologgi.numerocast");
-    const locale = await getLocale();
-    const today = new Date().toLocaleDateString(locale, {
-        day: "numeric",
-        month: "long",
-    });
+    const [t, locale] = await Promise.all([
+        getTranslations("numerologgi.numerocast"),
+        getLocale(),
+    ]);
+
     const numerocast = await fetchNumerocast();
-    const hasVideo = numerocast?.video && numerocast.video.url;
 
     if (!numerocast) {
         return NotFound();
     }
 
+    const today = new Date().toLocaleDateString(locale, {
+        day: "numeric",
+        month: "long",
+    });
+
+    const videoUrl = numerocast.video?.url;
 
     return (
-        <section className="w-full flex flex-col max-w-[450px] gap-6 pb-12">
-            <HeaderSection title={t("title")} date={numerocast?.date || today} />
+        <section className="w-full flex flex-col max-w-[500px] gap-6 pb-12">
+            <HeaderSection date={numerocast.date || today} authorLabel={t("authors")} />
 
             <div className="flex flex-col justify-center gap-4">
-                {hasVideo && (
+                {videoUrl ? (
                     <ShortVideoPlayer
-                        className="h-[75dvh]  aspect-9/16 rounded-3xl shadow-2xl border border-white/10"
-                        src={numerocast.video?.url || ""}
+                        className="w-full aspect-9/16 rounded-3xl shadow-2xl h-[70vh] border border-white/10"
+                        src={videoUrl}
                         poster={numerocast.cover.url || ""}
-                        userName="numerologgi"
+                        userName={{
+                            name: "numerologgi",
+                            url: "https://numerologgi.com",
+                        }}
                     />
+                ) : (
+                    <RecommendationList recommendations={numerocast.recommendations} />
                 )}
-                <RecommendationList recommendations={numerocast.recommendations} />
             </div>
+
+
         </section>
     );
 }
